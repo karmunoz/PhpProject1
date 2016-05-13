fadd63392ac9f066b7b708ad664f52b1e895c8be
Karen <karenelizabetm@gmail.com>
2016-05-13 12:50:38 -0300

    xambio de tamaño en triples

--- triples/V-C-C.php
+++ triples/V-C-C.php
@@ -2,7 +2,7 @@
 <?php 
     $algo =$_REQUEST['valor'];
 ?>
-<div class="btn-group" id="<?php echo $algo; ?>">
+<div class="btn-group" id="<?php echo $algo; ?>" style="width:255px; height: 130px;">
     <div class="thumbnail" id="<?php echo $algo; ?>_TRIPLE" ><strong>TRIPLE PATTERN  V-C-C</strong>
     	<button type="button" class="close" aria-hidden="true" onclick="borrarPanel(<?php echo $algo; ?>)">&times;</button>
     	<br> 
